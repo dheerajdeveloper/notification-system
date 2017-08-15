575653b29c9351d8a6307d1941692c4814740b4f
Dheeraj Tyagi <dheeraj.tyagi@paytm.com>
2017-08-15 22:21:44 +0530

    Adding code for logictics architecture

---
+++ src/main/java/com/notification/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.notification.repository;
+
+import com.notification.model.Item;
+import com.notification.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by dheeraj on 15/08/17.
+ */
+
+@RepositoryRestResource
+public interface UserRepository extends CrudRepository<User, Long>{
+}
